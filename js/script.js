27c5f48664a152ac16a5f1106a990b6524f9d852
nhatnam232 <namne2520@gmail.com>
2026-04-18 17:12:31 +0700

    Rename bot and website to Jsecurity

--- js/script.js
+++ js/script.js
@@ -45,7 +45,8 @@ function createCategoryButton(key, category) {
                     <span>${category.title}</span>
                 </div>
                 <svg class="w-6 h-6 transform transition-transform" id="${key}-arrow"
-                     xmlns="http:
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                 </svg>
             </button>
